25c35767f7cb9401ef0eaac7ca9955a48f32f878
Tithi1408 <80686777+Tithi1408@users.noreply.github.com>
2022-01-14 21:43:32 +0530

    Add files via upload

---
+++ Random Number genrator.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<time.h>
+
+// Driver program
+int main(void)
+{
+
+	srand(time(0));
+
+	for(int i = 0; i<4; i++)
+		printf(" %d ", rand());
+
+	return 0;
+}
+
